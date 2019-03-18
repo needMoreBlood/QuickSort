677681da1dd78f4b599539fad9f9efc3baec1991
needMoreBlood <fedulova.lena2051@gmail.com>
2019-03-18 19:44:17 +0500

    Написала код для быстрой сортировки и тесты к нему

--- ClassLibrary5/Class1.cs
+++ ClassLibrary5/Class1.cs
@@ -4,5 +4,49 @@ namespace ClassLibrary5
 {
     public class Class1
     {
+        public static void HoareSort(int[] array, int start, int end)
+        {
+            if (end == start) return;
+            var pivot = array[end];
+            var storeIndex = start;
+            for (int i = start; i <= end - 1; i++)
+                if (array[i] <= pivot)
+                {
+                    var t = array[i];
+                    array[i] = array[storeIndex];
+                    array[storeIndex] = t;
+                    storeIndex++;
+                }
+
+            var n = array[storeIndex];
+            array[storeIndex] = array[end];
+            array[end] = n;
+            if (storeIndex > start) HoareSort(array, start, storeIndex - 1);
+            if (storeIndex < end) HoareSort(array, storeIndex + 1, end);
+        }
+
+        public static void QuickSort(int[] array)
+        {
+            if (array.Length != 0)
+                HoareSort(array, 0, array.Length - 1);
+        }
+
+        static Random random = new Random();
+
+        public static int[] GenerateArray(int length)
+        {
+            var array = new int[length];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next();
+            return array;
+        }
+
+        public static void Main()
+        {
+            var array = GenerateArray(10);
+            QuickSort(array);
+            foreach (var e in array)
+                Console.WriteLine(e);
+        }
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary5;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ThreeElements()
+        {
+            var array = new int[] { 10, 34, 2 };
+            Class1.QuickSort(array);
+
+            for (int i = 0; i < array.Length - 1; i++)
+                Assert.IsTrue(array[i] <= array[i + 1]);
+        }
+
+        [TestMethod]
+        public void OneHundredElements()
+        {
+            var array = new int[100];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 56;
+            }
+
+            Class1.QuickSort(array);
+
+            for (int i = 0; i < 99; i++)
+            {
+                Assert.IsTrue(array[i] <= array[i + 1]);
+            }
+        }
+
+        [TestMethod]
+        public void OneThousandElements()
+        {
+            var rnd = new Random(56);
+
+            var array = new int[1000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next();
+            }
+
+            Class1.QuickSort(array);
+
+            for (int j = 0; j < 10; j++)
+            {
+                int e, w;
+
+                do
+                {
+                    e = rnd.Next(0, 999);
+                    w = rnd.Next(0, 999);
+                } while (e >= w);
+
+                Assert.IsTrue(array[e] < array[w]);
+            }
+        }
+
+
+        [TestMethod]
+        public void EmptyArray()
+        {
+            var array = new int[] { };
+
+            Class1.QuickSort(array);
+            Assert.AreEqual(0, array.Length);
+        }
+
+        [TestMethod]
+        public void ManyElements()
+        {
+            var array = Class1.GenerateArray(150000000);
+            Class1.QuickSort(array);
+
+            for (int i = 0; i < 150000000 - 1; i++)
+            {
+                Assert.IsTrue(array[i] <= array[i + 1]);
+            }
+        }
+    }
+}
